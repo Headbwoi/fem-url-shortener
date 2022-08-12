60280bad18e0946e89b093be6e4851c64ee50647
headbwoi <tochukwujohn24@gmail.com>
2022-08-12 04:39:31 +0100

    configured tailwind, arranged folders

---
+++ tailwind.config.cjs
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{ts.tsx}"],
+  theme: {
+    screens: {
+      sm: "540px",
+      md: "768px",
+      lg: "1112px",
+    },
+    container: {
+      center: true,
+    },
+    colors: {
+      Cyan: "hsl(180, 66%, 49%)",
+      Dark_Violet: "hsl(257, 27%, 26%)",
+      Red: "hsl(0, 87%, 67%)",
+      Gray: "hsl(0, 0%, 75%)",
+      Grayish_Violet: "hsl(257, 7%, 63%)",
+      Very_Dark_Blue: "hsl(255, 11%, 22%)",
+      Very_Dark_Violet: "hsl(260, 8%, 14%)",
+    },
+    fontFamily: {
+      poppins: [`"Poppins", sans-serif`],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
