53d283e2d68f4ca022292f8e6648e94d1cc91d1e
headbwoi <tochukwujohn24@gmail.com>
2022-08-18 12:07:02 +0100

    fixed type bug on url components

--- src/components/Main/ShortenedLinkCard.tsx
+++ src/components/Main/ShortenedLinkCard.tsx
@@ -1,26 +1,42 @@
-import { useState } from "react"
-import { Props } from "./UrlShortener"
+import { useEffect, useState } from "react"
 
-const ShortenedLinkCard = (props: Props) => {
+const ShortenedLinkCard = (props: any) => {
+  const data = props.data
+  let oldLink = data.original_link
+  let newLink = data.full_short_link
   const [copied, setCopied] = useState(false)
-  const flipBtn = () => {
+
+  const copyLink = async () => {
+    await navigator.clipboard.writeText(newLink)
     setCopied(true)
   }
+  useEffect(() => {
+    if (!copied) return
+
+    const timeout = setTimeout(() => {
+      setCopied(false)
+    }, 4000)
+    return () => clearTimeout(timeout)
+  }, [copied])
   return (
     <div className="h-40 md:h-[4.625rem] flex items-center justify-center md:justify-between flex-col md:flex-row py-2 md:py-0 bg-white px-[1.125rem] md:px-8">
       {/* old link */}
       <div className=" border-b border-Gray md:border-none w-full">
-        <p className="text-Very_Dark_Violet pb-2 md:pb-0">{props.oldLink}</p>
+        <a href={oldLink} className="text-Very_Dark_Violet pb-2 md:pb-0">
+          {oldLink}
+        </a>
       </div>
       {/* new link and btn */}
       <div className="pt-2 md:pt-0 flex md:items-center flex-col md:flex-row w-full md:w-auto md:space-x-6">
-        <p className="text-Cyan text-left mb-2 md:mb-0">{props.newLink}</p>
+        <a href={newLink} className="text-Cyan text-left mb-2 md:mb-0">
+          {newLink}
+        </a>
 
         <button
           className={`${
             copied ? "cta-tertiary" : "cta-secondary"
           } h-10 md:h-11 w-full md:w-[6.625rem] font-normal`}
-          onClick={flipBtn}
+          onClick={copyLink}
         >
           {copied ? "Copied" : "Copy"}
         </button>
--- src/components/Main/UrlShortener.tsx
+++ src/components/Main/UrlShortener.tsx
@@ -1,32 +1,45 @@
-import { useState } from "react"
-import { CopyCard } from "./ShortenedLinkCard"
+import { useEffect, useState } from "react"
+import ShortenedLinkCard from "./ShortenedLinkCard"
 
 export interface Props {
   oldLink: string
   newLink?: string
 }
-export const UrlShortener = () => {
+const UrlShortener = () => {
+  const getStoredLinks = JSON.parse(localStorage.getItem("links") || "")
   const [urlValue, setUrlValue] = useState("")
   const [error, setError] = useState(false)
+  const [storedLinks, setStoredLinks] = useState(getStoredLinks)
 
-  const handleUrlInput = (e: any) => {
+  useEffect(() => {
+    localStorage.setItem("links", JSON.stringify(storedLinks))
+  }, [storedLinks])
+
+  const shortenLink = async (e: any) => {
     e.preventDefault()
-  }
-  const validateInput = () => {
     urlValue === "" ? setError(true) : setError(false)
+    let editedUrl = urlValue.replaceAll(" ", "")
+
+    const res = await fetch(`https://api.shrtco.de/v2/shorten?url=${editedUrl}`)
+    const responseData = await res.json()
+    responseData.ok
+      ? setStoredLinks([...storedLinks, responseData.result])
+      : setError(true)
   }
+
   return (
     <div className="url-container md:w-full absolute -top-[6.6875rem] md:-top-[5.3125rem] left-1/2 -translate-x-1/2  overflow-x-hidden">
       <div
         className="rounded-lg h-[13.375rem] md:h-[10.625rem] bg-Dark_Violet bg-[url('./assets/bg-shorten-mobile.svg')] md:bg-[url('./assets/bg-shorten-desktop.svg')] bg-no-repeat bg-right-top md:bg-center mb-5  flex items-center justify-between px-6 md:px-16"
-        onSubmit={handleUrlInput}
+        onSubmit={shortenLink}
       >
         <form className="flex items-center justify-between flex-col md:flex-row relative w-full">
           <div className="w-full block mb-5 md:mb-0 relative">
             <input
-              type="url"
+              type="text"
               name="url"
               value={urlValue}
+              onFocus={() => setError(false)}
               className={`w-full md:w-[78%] lg:w-[48.125rem] h-16 rounded-lg px-8 mb-4 md:mb-0 outline-none ${
                 error == true ? "border-4 border-Red" : "border-none"
               }`}
@@ -34,7 +47,7 @@ export const UrlShortener = () => {
               onChange={(e) => setUrlValue(e.target.value)}
             />
             {error && (
-              <p className="text-base text-Red italic absolute top-full mt-1">
+              <p className="text-base text-Red italic absolute -bottom-4 md:-bottom-7">
                 Please add a link
               </p>
             )}
@@ -42,7 +55,6 @@ export const UrlShortener = () => {
           <button
             type="submit"
             className="w-full md:w-[11.875rem] h-12 md:h-16 cta-secondary rounded-md"
-            onClick={validateInput}
           >
             Shorten It!
           </button>
@@ -51,11 +63,14 @@ export const UrlShortener = () => {
       {/* links preview */}
 
       <div className="w-full space-y-4">
-        <CopyCard
-          oldLink="https://frontendmentor.io"
-          newLink="https://frontendmentor.io"
-        />
+        {storedLinks.length !== 0 &&
+          storedLinks.map((link: any) => {
+            return (
+              <ShortenedLinkCard key={storedLinks.indexOf(link)} data={link} />
+            )
+          })}
       </div>
     </div>
   )
 }
+export default UrlShortener
