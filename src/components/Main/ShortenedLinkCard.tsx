d73d43f16fcb65904937d34da77c2196dee065f0
headbwoi <tochukwujohn24@gmail.com>
2022-08-20 04:52:57 +0100

    fixed url failed to fetch error

--- src/components/Main/ShortenedLinkCard.tsx
+++ src/components/Main/ShortenedLinkCard.tsx
@@ -12,7 +12,6 @@ const ShortenedLinkCard = (props: any) => {
   }
   useEffect(() => {
     if (!copied) return
-
     const timeout = setTimeout(() => {
       setCopied(false)
     }, 4000)
@@ -22,13 +21,13 @@ const ShortenedLinkCard = (props: any) => {
     <div className="h-40 md:h-[4.625rem] flex items-center justify-center md:justify-between flex-col md:flex-row py-2 md:py-0 bg-white px-[1.125rem] md:px-8">
       {/* old link */}
       <div className=" border-b border-Gray md:border-none w-full">
-        <a href={oldLink} className="text-Very_Dark_Violet pb-2 md:pb-0">
+        <a href={oldLink} className="text-Very_Dark_Violet pb-2 md:pb-0 block">
           {oldLink}
         </a>
       </div>
       {/* new link and btn */}
       <div className="pt-2 md:pt-0 flex md:items-center flex-col md:flex-row w-full md:w-auto md:space-x-6">
-        <a href={newLink} className="text-Cyan text-left mb-2 md:mb-0">
+        <a href={newLink} className="text-Cyan text-left mb-2 md:mb-0 block">
           {newLink}
         </a>
 
--- src/components/Main/UrlShortener.tsx
+++ src/components/Main/UrlShortener.tsx
@@ -19,55 +19,61 @@ const UrlShortener = () => {
 
     const res = await fetch(`https://api.shrtco.de/v2/shorten?url=${editedUrl}`)
     const responseData = await res.json()
+    console.log(responseData)
     responseData.ok
       ? setStoredLinks([...storedLinks, responseData.result])
       : setError(true)
+    setUrlValue("")
   }
 
   return (
-    <div className="url-container md:w-full absolute -top-[6.6875rem] md:-top-[5.3125rem] left-1/2 -translate-x-1/2  overflow-x-hidden">
-      <div
-        className="rounded-lg h-[13.375rem] md:h-[10.625rem] bg-Dark_Violet bg-[url('./assets/bg-shorten-mobile.svg')] md:bg-[url('./assets/bg-shorten-desktop.svg')] bg-no-repeat bg-right-top md:bg-center mb-5  flex items-center justify-between px-6 md:px-16"
-        onSubmit={shortenLink}
-      >
-        <form className="flex items-center justify-between flex-col md:flex-row relative w-full">
-          <div className="w-full block mb-5 md:mb-0 relative">
-            <input
-              type="text"
-              name="url"
-              value={urlValue}
-              onFocus={() => setError(false)}
-              className={`w-full md:w-[78%] lg:w-[48.125rem] h-16 rounded-lg px-8 mb-4 md:mb-0 outline-none ${
-                error == true ? "border-4 border-Red" : "border-none"
-              }`}
-              placeholder="Shorten a link here..."
-              onChange={(e) => setUrlValue(e.target.value)}
-            />
-            {error && (
-              <p className="text-base text-Red italic absolute -bottom-4 md:-bottom-7">
-                Please add a link
-              </p>
-            )}
-          </div>
-          <button
-            type="submit"
-            className="w-full md:w-[11.875rem] h-12 md:h-16 cta-secondary rounded-md"
-          >
-            Shorten It!
-          </button>
-        </form>
+    <>
+      <div className="url-container md:w-full absolute -top-[6.6875rem] md:-top-[5.3125rem] left-1/2 -translate-x-1/2  overflow-x-hidden">
+        <div
+          className="rounded-lg h-[13.375rem] md:h-[10.625rem] bg-Dark_Violet bg-[url('./assets/bg-shorten-mobile.svg')] md:bg-[url('./assets/bg-shorten-desktop.svg')] bg-no-repeat bg-right-top md:bg-center mb-5  flex items-center justify-between px-6 md:px-16"
+          onSubmit={shortenLink}
+        >
+          <form className="flex items-center justify-between flex-col md:flex-row relative w-full">
+            <div className="w-full block mb-5 md:mb-0 relative">
+              <input
+                type="text"
+                name="url"
+                value={urlValue}
+                onFocus={() => setError(false)}
+                className={`w-full md:w-[78%] lg:w-[48.125rem] h-16 rounded-lg px-8 mb-4 md:mb-0 outline-none ${
+                  error == true ? "border-4 border-Red" : "border-none"
+                }`}
+                placeholder="Shorten a link here..."
+                onChange={(e) => setUrlValue(e.target.value)}
+              />
+              {error && (
+                <p className="text-base text-Red italic absolute -bottom-4 md:-bottom-7">
+                  Please add a link
+                </p>
+              )}
+            </div>
+            <button
+              type="submit"
+              className="w-full md:w-[11.875rem] h-12 md:h-16 cta-secondary rounded-md"
+            >
+              Shorten It!
+            </button>
+          </form>
+        </div>
+        {/* links preview */}
+        <div className="w-full space-y-4">
+          {storedLinks &&
+            storedLinks.map((link: any) => {
+              return (
+                <ShortenedLinkCard
+                  key={storedLinks.indexOf(link)}
+                  data={link}
+                />
+              )
+            })}
+        </div>
       </div>
-      {/* links preview */}
-
-      <div className="w-full space-y-4">
-        {storedLinks &&
-          storedLinks.map((link: any) => {
-            return (
-              <ShortenedLinkCard key={storedLinks.indexOf(link)} data={link} />
-            )
-          })}
-      </div>
-    </div>
+    </>
   )
 }
 export default UrlShortener
